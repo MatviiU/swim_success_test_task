86bd242dabfdcf79f2fcc9573a19a20e7f0f18b2
Matvii Ulytskyi <matviiulytskyi@MacBook-Air-Matvii.local>
2026-06-20 13:03:10 +0200

    fix(core): fixed wrong routes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:swim_success/core/di/injector.dart';
+import 'package:swim_success/core/router/app_routes.dart';
 import 'package:swim_success/features/user_list/domain/entities/user_entity.dart';
 import 'package:swim_success/features/user_list/presentation/cubits/user_list_cubit.dart';
 import 'package:swim_success/features/user_list/presentation/pages/user_detail_page.dart';
 import 'package:swim_success/features/user_list/presentation/pages/user_list_page.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: AppRoutes.users,
   routes: [
     GoRoute(
-      path: '/',
+      path: AppRoutes.users,
       builder: (context, state) => BlocProvider(
         create: (context) => getIt<UserListCubit>()..getUsers(),
         child: const UserListPage(),
